ab85d3f846cb5e3fcdf986f3ecc2ba358ca6f1d6
pmayur <thisispatil@gmail.com>
2020-12-14 22:53:24 +0530

    add: styling to navigation links

--- src/components/navigation/Navigator.jsx
+++ src/components/navigation/Navigator.jsx
@@ -3,27 +3,29 @@ import { Link } from "react-router-dom";
 import "./Navigator.css";
 
 export default function Navigator() {
+    const TEXTDECOR = { textDecoration: 'none' };
+
     return (
         <div id="navigator">
-            <div id="list">
-                <Link to="/">
-                    <div>i am</div>
+            <div id="link-list">
+                <Link to="/" style={TEXTDECOR}>
+                    <div className="link">i am</div>
                 </Link>
 
-                <Link to="/work">
-                    <div>work</div>
+                <Link to="/work" style={TEXTDECOR}>
+                    <div className="link">work</div>
                 </Link>
 
-                <Link to="/education">
-                    <div>education</div>
+                <Link to="/education" style={TEXTDECOR}>
+                    <div className="link">education</div>
                 </Link>
 
-                <Link to="/projects">
-                    <div>projects</div>
+                <Link to="/projects" style={TEXTDECOR}>
+                    <div className="link">projects</div>
                 </Link>
 
-                <Link to="/aboutme">
-                    <div>about me</div>
+                <Link to="/aboutme" style={TEXTDECOR}>
+                    <div className="link">about me</div>
                 </Link>
             </div>
         </div>
