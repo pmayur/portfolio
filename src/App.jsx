8681d5579b7ab56a43fc398ff2145d9ced5d4f93
pmayur <thisispatil@gmail.com>
2020-12-14 22:12:51 +0530

    add: switch to a placeholder view

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import Navigator from "./components/navigation/Navigator"
+import Navigator    from "./components/navigation/Navigator";
+import View         from "./components/views/View";
+
+import { MemoryRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <Navigator />
-        </div>
+        <Router>
+            <div className="App">
+                <Navigator />
+
+                <Switch>
+                    <Route
+                        exact path="/"
+                        component={() => <View name="me" />}
+                    />
+                    <Route
+                        path="/work"
+                        component={() => <View name="work" />}
+                    />
+                    <Route
+                        path="/education"
+                        component={() => <View name="education" />}
+                    />
+                    <Route
+                        path="/projects"
+                        component={() => <View name="projects" />}
+                    />
+                    <Route
+                        path="/aboutme"
+                        component={() => <View name="about" />}
+                    />
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
