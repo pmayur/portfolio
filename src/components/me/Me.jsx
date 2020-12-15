dced1ccce22b26e4eb099c1959af1cdee377fcda
pmayur <thisispatil@gmail.com>
2020-12-15 17:29:09 +0530

    add: instance of logo component in me component

--- src/components/me/Me.jsx
+++ src/components/me/Me.jsx
@@ -1,4 +1,7 @@
-import "./Me.css"
+import "./Me.css";
+
+import htmlIcon from "./images/html5-2.svg";
+import Icon from "./icon/Icon";
 
 export default function Me() {
     return (
@@ -7,7 +10,12 @@ export default function Me() {
                 <h1 id="name">mayur patil</h1>
                 <h2 id="desig">full stack engineer</h2>
             </div>
-
+            <div id="skill">
+                <h3>things i like working on</h3>
+                <div id="icons">
+                    <Icon image={htmlIcon} text={"HTML "} />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
