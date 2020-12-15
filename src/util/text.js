bc57d2fd92d8821971120582b334c70ffc3d3c8a
pmayur <thisispatil@gmail.com>
2020-12-15 23:51:51 +0530

    add: project summary details to use

--- src/util/text.js
+++ src/util/text.js
@@ -1,6 +1,6 @@
 const MONKEYCHAI = {
     TITLE: "Junior Software Engineer",
-    COMPANY:"MonkeyChai",
+    COMPANY: "MonkeyChai",
     DESC: "Software Design and Development Consultancy",
     DATE: "(September 2019 - July 2020)",
     LIST: [
@@ -18,9 +18,9 @@ const ECW = {
     LIST: [
         "Reached out to Healthcare Practices which reported Issues with application Post Go Live",
         "Identified root cause and resolved issues with detailed documentation, while working with cross functional teams",
-        "Identified bugs, studied behavior in different environments/versions, worked with development teams to resolve them"
-    ]
-}
+        "Identified bugs, studied behavior in different environments/versions, worked with development teams to resolve them",
+    ],
+};
 
 const HASURA = {
     TITLE: "Web Development Intern",
@@ -29,8 +29,73 @@ const HASURA = {
     DATE: "(June 2017 - July 2017)",
     LIST: [
         "Built application wire-frames for a web application using prototyping tool - proto.io",
-        "Converted the wire-frame into a functional SPA using Vue JS with early stage Hasura serving as a backend"
-    ]
-}
+        "Converted the wire-frame into a functional SPA using Vue JS with early stage Hasura serving as a backend",
+    ],
+};
 
-export { MONKEYCHAI, ECW, HASURA };
+const BACKEND = [
+    {
+        TITLE: "URL Shortener Microservice",
+        DESC: [
+            "built with express using mongodb as a database",
+            "returns a random string which redirects to the url passed",
+        ],
+    },
+    {
+        TITLE: "Anonymous Message Board",
+        DESC: [
+            "test driven development using express, mongoose and chai",
+            "users can create threads in any board and reply to the threads",
+        ],
+    },
+    {
+        TITLE: "Issue Tracker Microservice",
+        DESC: [
+            "test driven development using express, mongoose and chai",
+            "users can create tasks with details and assign it to other users",
+        ],
+    },
+];
+
+const FRONTEND = [
+    {
+        TITLE: "Redux React Calculator",
+        DESC: [
+            "built with react for front end and redux as state management",
+            "performs all basic operations using formula logic",
+        ],
+    },
+    {
+        TITLE: "React Pomodoro clock",
+        DESC: [
+            "built with react for front end and redux as state management",
+            "ability to set times with breaks in between",
+        ],
+    },
+    {
+        TITLE: "Choropleth map for data visualization",
+        DESC: [
+            "used javascript d3 library to display a choropleth",
+            "used open source data to create US map and visualized data about no of graduates in US",
+        ],
+    },
+];
+
+const ANDROID = [
+    {
+        TITLE: "Crypto Currency portfolio tracker",
+        DESC: [
+            "used flutter and firebase along with third party api to keep track of cryto currency prices",
+            "authenticated users can create a porfolio of currencies and keep track of prices",
+        ],
+    },
+    {
+        TITLE: "Habits tracker",
+        DESC: [
+            "used react native to develop the application",
+            "users can create daily recurring tasks and perform mark complete / delete operations",
+        ],
+    },
+];
+
+export { MONKEYCHAI, ECW, HASURA, BACKEND, FRONTEND, ANDROID };
