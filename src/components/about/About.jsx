1e15c4e0d4dd779007e5c949de93f7a1295666fd
pmayur <thisispatil@gmail.com>
2020-12-16 00:22:16 +0530

    add: about container files

---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import "./About.css"
+
+export default function About() {
+    return (
+        <div id="about-container">
+            <div>
+                ABOUT
+            </div>
+        </div>
+    )
+}
