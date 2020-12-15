a59386216de20279907f2a93f3fbea328910df30
pmayur <thisispatil@gmail.com>
2020-12-15 23:52:38 +0530

    add: project component implementation

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,11 +1,40 @@
-import "./Projects.css"
+import "./Projects.css";
+import { BACKEND, FRONTEND, ANDROID } from "../../util/text";
 
 export default function Projects() {
+    let backendProjects = createProjectList(BACKEND);
+    let frontendProjects = createProjectList(FRONTEND);
+    let androidProjects = createProjectList(ANDROID)
+
+
     return (
         <div id="project-container">
-            <div id="project">
-                PROJECTS
+            <div id="container">
+                <div className="project">
+                    <h2 className="project-section">Back End Focused</h2>
+                    {backendProjects}
+                </div>
+                <div className="project">
+                    <h2 className="project-section">Front End Focused</h2>
+                    {frontendProjects}
+                </div>
+                <div className="project">
+                    <h2 className="project-section">Android Apps</h2>
+                    {androidProjects}
+                </div>
             </div>
         </div>
     );
 }
+
+const createProjectList = (list) => {
+    return list.map((project, index) => {
+        return (
+            <div key={index}>
+                <h4 className="project-title">{project.TITLE}</h4>
+                <p className="project-desc">{project.DESC[0]}</p>
+                <p className="project-desc">{project.DESC[1]}</p>
+            </div>
+        );
+    });
+};
