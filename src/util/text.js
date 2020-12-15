8ad74a9beb1e2d24c3b3eda0b5f76ce391add295
pmayur <thisispatil@gmail.com>
2020-12-15 20:57:01 +0530

    add: file containing work summaries

---
+++ src/util/text.js
@@ -0,0 +1,36 @@
+const MONKEYCHAI = {
+    TITLE: "Junior Software Engineer",
+    COMPANY:"MonkeyChai",
+    DESC: "Software Design and Development Consultancy",
+    DATE: "(September 2019 - July 2020)",
+    LIST: [
+        "Analysed requirements, discussed system design, researched viability of using software frameworks for new projects",
+        "Converted mockups to code following Test Driven Approaches for web applications using Ruby on Rails",
+        "Developed an Android application using Flutter from a UI specification for financial analytics client",
+    ],
+};
+
+const ECW = {
+    TITLE: "Technical Account Manager",
+    COMPANY: "eClinicalWorks",
+    DESC: "Electronic Health and Medical Record System Provider",
+    DATE: "(June 2018 - July 2019)",
+    LIST: [
+        "Reached out to Healthcare Practices which reported Issues with application Post Go Live",
+        "Identified root cause and resolved issues with detailed documentation, while working with cross functional teams",
+        "Identified bugs, studied behavior in different environments/versions, worked with development teams to resolve them"
+    ]
+}
+
+const HASURA = {
+    TITLE: "Web Development Intern",
+    COMPANY: "Hasura",
+    DESC: "Backend as a service provider",
+    DATE: "(June 2017 - July 2017)",
+    LIST: [
+        "Built application wire-frames for a web application using prototyping tool - proto.io",
+        "Converted the wire-frame into a functional SPA using Vue JS with early stage Hasura serving as a backend"
+    ]
+}
+
+export { MONKEYCHAI, ECW, HASURA };
