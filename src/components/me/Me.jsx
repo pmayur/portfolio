f49f21685a42fb27ec34b0c52c75f936b8aad166
pmayur <thisispatil@gmail.com>
2020-12-15 16:36:42 +0530

    add: me component with just name and designation

---
+++ src/components/me/Me.jsx
@@ -0,0 +1,13 @@
+import "./Me.css"
+
+export default function Me() {
+    return (
+        <div id="me">
+            <div id="heading">
+                <h1 id="name">mayur patil</h1>
+                <h2 id="desig">full stack engineer</h2>
+            </div>
+
+        </div>
+    )
+}
