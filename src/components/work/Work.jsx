a20dc9f6f79bf106effa4dee09468f1fea4f9ba3
pmayur <thisispatil@gmail.com>
2020-12-15 20:58:12 +0530

    add: work component for work summary

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,6 +1,19 @@
-import "./Work.css"
+import Company from "./company/Company";
+import "./Work.css";
+import { MONKEYCHAI, ECW, HASURA } from "./../../util/text";
+
 export default function Work() {
     return (
-        <div id="work">WORK</div>
-    )
+        <div id="work-container">
+            <div id="container">
+                <Company company={MONKEYCHAI} />
+
+                <br />
+                <Company company={ECW} />
+
+                <br />
+                <Company company={HASURA} />
+            </div>
+        </div>
+    );
 }
