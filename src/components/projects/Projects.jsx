d8d9369ccd8909525565298f113937afd86e06c3
pmayur <thisispatil@gmail.com>
2020-12-15 22:17:41 +0530

    add: project component files

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,11 @@
+import "./Projects.css"
+
+export default function Projects() {
+    return (
+        <div id="project-container">
+            <div id="project">
+                PROJECTS
+            </div>
+        </div>
+    );
+}
