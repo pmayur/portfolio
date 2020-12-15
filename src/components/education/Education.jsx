4920917033782d6423c7e204ea817bc21b653495
pmayur <thisispatil@gmail.com>
2020-12-15 21:58:31 +0530

    add: summary in education component

--- src/components/education/Education.jsx
+++ src/components/education/Education.jsx
@@ -1,5 +1,34 @@
 import "./Education.css";
 
 export default function Education() {
-    return <div id="education">EDUCATION</div>;
+    return (
+        <div id="education-container">
+            <div id="education">
+
+                <h2 id="education-title">Full Stack Developer Certification</h2>
+                <h3 id="education-desc">FreeCodeCamp</h3>
+                <div>
+                    <p className="education-para">
+                        Open Source Curriculum comprising of 6 Certifications
+                        covering different aspects of development
+                    </p>
+                    <p className="education-para">
+                        Course work comprising of of 100's of hours of exercises
+                        and 30+ mini projects
+                    </p>
+                </div>
+
+                <br />
+                <br />
+
+                <h2 id="education-title">
+                    Bachelor of Engineering, Computer Science
+                </h2>
+                <h3 id="education-desc">
+                    University of Mumbai - 6.86/10{" "}
+                    <span id="education-date">(2014 - 2018)</span>
+                </h3>
+            </div>
+        </div>
+    );
 }
