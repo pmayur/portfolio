778a7e908be2ed3b557aa0fd746dce63aabe4338
pmayur <thisispatil@gmail.com>
2020-12-15 17:50:27 +0530

    add: complete me component draft

--- src/components/me/Me.jsx
+++ src/components/me/Me.jsx
@@ -1,8 +1,5 @@
 import "./Me.css";
 
-import htmlIcon from "./images/html5-2.svg";
-import Icon from "./icon/Icon";
-
 export default function Me() {
     return (
         <div id="me">
@@ -11,10 +8,12 @@ export default function Me() {
                 <h2 id="desig">full stack engineer</h2>
             </div>
             <div id="skill">
-                <h3>things i like working on</h3>
-                <div id="icons">
-                    <Icon image={htmlIcon} text={"HTML "} />
-                </div>
+                <h3>
+                    likes building applications with express, react, mongodb
+                </h3>
+                <h3>
+                    have worked on java, ruby, rails, flutter
+                </h3>
             </div>
         </div>
     );
