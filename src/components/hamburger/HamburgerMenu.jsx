54e316f52d192dd2d30682b12951832b82e5fad9
pmayur <thisispatil@gmail.com>
2020-12-16 23:22:56 +0530

    update: hamburger menu disappears on navig

--- src/components/hamburger/HamburgerMenu.jsx
+++ src/components/hamburger/HamburgerMenu.jsx
@@ -1,31 +1,27 @@
-import React, { useState } from 'react'
-import "./HamburgerMenu.css"
-import hamburgerIcon from "../../assets/hamburger.png"
+import React, { useState } from "react";
+import "./HamburgerMenu.css";
+import hamburgerIcon from "../../assets/hamburger.png";
 
-import Navigator from "../navigation/Navigator"
+import Navigator from "../navigation/Navigator";
 
 export default function HamburgerMenu() {
-
     const [menuVisibility, setmenuVisibility] = useState(false);
 
-    const showMenu = () => {
-        setmenuVisibility(true);
-    }
+    const showMenu = () => setmenuVisibility(true);
 
-    const hideMenu = () => {
-        setmenuVisibility(false);
-    }
+    const hideMenu = () => setmenuVisibility(false);
 
     return (
         <div id="hamburger-menu-container">
-            <div id="hamburger-icon-wrapper" onClick={ menuVisibility ? hideMenu : showMenu }>
-                <img src={hamburgerIcon} alt="" id="hamburger-icon"/>
+            <div
+                id="hamburger-icon-wrapper"
+                onClick={menuVisibility ? hideMenu : showMenu}
+            >
+                <img src={hamburgerIcon} alt="" id="hamburger-icon" />
             </div>
-            <div id="navig-menu-wrapper">
-                {
-                    menuVisibility ? <Navigator /> : null
-                }
+            <div id="navig-menu-wrapper" onClick={ hideMenu }>
+                {menuVisibility ? <Navigator /> : null}
             </div>
         </div>
-    )
+    );
 }
