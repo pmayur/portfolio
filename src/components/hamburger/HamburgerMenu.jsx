46443859d728e5a5fcaec791cee4bd99f9010e4a
pmayur <thisispatil@gmail.com>
2020-12-16 23:02:15 +0530

    add: view hinde menu on clicking menu icon

--- src/components/hamburger/HamburgerMenu.jsx
+++ src/components/hamburger/HamburgerMenu.jsx
@@ -1,16 +1,30 @@
+import React, { useState } from 'react'
 import "./HamburgerMenu.css"
 import hamburgerIcon from "../../assets/hamburger.png"
 
 import Navigator from "../navigation/Navigator"
 
 export default function HamburgerMenu() {
+
+    const [menuVisibility, setmenuVisibility] = useState(false);
+
+    const showMenu = () => {
+        setmenuVisibility(true);
+    }
+
+    const hideMenu = () => {
+        setmenuVisibility(false);
+    }
+
     return (
         <div id="hamburger-menu-container">
-            <div id="hamburger-icon-wrapper">
+            <div id="hamburger-icon-wrapper" onClick={ menuVisibility ? hideMenu : showMenu }>
                 <img src={hamburgerIcon} alt="" id="hamburger-icon"/>
             </div>
             <div id="navig-menu-wrapper">
-                <Navigator />
+                {
+                    menuVisibility ? <Navigator /> : null
+                }
             </div>
         </div>
     )
