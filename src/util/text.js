ddd72e32f1be96cce836695603a62327d7d0471f
pmayur <thisispatil@gmail.com>
2020-12-16 23:36:14 +0530

    add: websites to summaries

--- src/util/text.js
+++ src/util/text.js
@@ -1,5 +1,6 @@
 const MONKEYCHAI = {
     TITLE: "Junior Software Engineer",
+    SITE: "https://monkeychai.com/",
     COMPANY: "MonkeyChai",
     DESC: "Software Design and Development Consultancy",
     DATE: "(September 2019 - July 2020)",
@@ -12,6 +13,7 @@ const MONKEYCHAI = {
 
 const ECW = {
     TITLE: "Technical Account Manager",
+    SITE: "https://www.eclinicalworks.com/",
     COMPANY: "eClinicalWorks",
     DESC: "Electronic Health and Medical Record System Provider",
     DATE: "(June 2018 - July 2019)",
@@ -24,6 +26,7 @@ const ECW = {
 
 const HASURA = {
     TITLE: "Web Development Intern",
+    SITE: "https://hasura.io/",
     COMPANY: "Hasura",
     DESC: "Backend as a service provider",
     DATE: "(June 2017 - July 2017)",
@@ -36,16 +39,19 @@ const HASURA = {
 const BACKEND = [
     {
         TITLE: "URL Shortener Microservice",
+        SITE: "https://github.com/pmayur/url-shortener-microservice",
         DESC:
             "a microservice using express and mongoose similar to tinyurl returns a short string, which passed as a param redirects to the original url",
     },
     {
         TITLE: "Anonymous Message Board",
+        SITE: "https://github.com/pmayur/fcc-is-anon-message-board",
         DESC:
             "used express and mongo and chai for testing to create a messaging board where anonymous users can create threads within boards and post replies, similar to anonymous image board 4chan",
     },
     {
         TITLE: "Issue Tracker Microservice",
+        SITE: "https://github.com/pmayur/fcc-qa-issue-tracker",
         DESC:
             "used express and mongo, following a test driven approach to create a service which creates and stores tasks which can be assigned to users",
     },
@@ -54,16 +60,19 @@ const BACKEND = [
 const FRONTEND = [
     {
         TITLE: "Redux React Calculator",
+        SITE: "https://github.com/pmayur/fcc-javascript-calculator",
         DESC:
             "used redux for state management along with react to create a calculator performing operations with formula logic",
     },
     {
         TITLE: "React Pomodoro clock",
+        SITE: "https://github.com/pmayur/fcc-25plus5-clock",
         DESC:
             "a pomodoro clock to keep track of sessions with breaks in between using react and redux",
     },
     {
         TITLE: "Choropleth map for data visualization",
+        SITE: "https://github.com/pmayur/fcc-dv-choropleth-map",
         DESC:
             "created a choropleth map using D3 JS for visualizing % of graduates per county in the US using openly available maps and data",
     },
@@ -72,11 +81,13 @@ const FRONTEND = [
 const ANDROID = [
     {
         TITLE: "Crypto Currency portfolio tracker",
+        SITE: "https://github.com/pmayur/crypto-currency-tracker",
         DESC:
             "authenticated users can create a portfolio of crypto currencies to keep track of prices, used flutter along with firebase for the app and third party API's for querying crypto prices",
     },
     {
         TITLE: "Habits tracker",
+        SITE: "https://github.com/pmayur/habits-app",
         DESC:
             "users can create recurring tasks and mark them complete, delete, postpone etc, used React Native for development",
     },
